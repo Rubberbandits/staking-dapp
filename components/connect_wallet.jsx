19375a654e1817a4afb9a2d0972aa4f3862de35a
rusty <dalegribble@riseup.net>
2021-10-19 17:16:49 -0700

    assure ethereum.selectedAddress is defined

--- components/connect_wallet.jsx
+++ components/connect_wallet.jsx
@@ -4,6 +4,8 @@ const ConnectButton = async () => {
 	const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
 	const account = accounts[0];
 
+	ethereum.selectedAddress = account;
+
 	var walletConnected = new CustomEvent("walletConnected", {detail: account});
 	window.dispatchEvent(walletConnected);
 }
